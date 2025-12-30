2458226684050df4bdb40983c6b8f6173830de37
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-30 13:51:29 +0330

    register ui added

--- app/_components/common/customInput/index.tsx
+++ app/_components/common/customInput/index.tsx
@@ -5,13 +5,14 @@ type Props = {
     name:string
     placeHolder?:string
     text:string
+    type?:"text"|"password"
 };
 
-const CustomInput = ({name,text,placeHolder}: Props) => {
+const CustomInput = ({name,text,placeHolder,type="text"}: Props) => {
   return (
-    <div className="grid w-full max-w-sm items-center gap-2">
+    <div className="grid w-full max-w-sm items-center gap-2 mb-2 "> 
       <Label htmlFor={name}>{text}</Label>
-      <Input id={name} name={name} placeholder={placeHolder||text} type="text" />
+      <Input  id={name} name={name} placeholder={placeHolder||text} type={type} />
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="da rk">
       <body
         className={`${InterFont.className}    antialiased`}
       >
