d8c3ba74d1af8dbb559e4e03d74fc43f546a58e6
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-31 11:40:31 +0330

    login action is done

--- app/_components/forms/loginForm/action.ts
+++ app/_components/forms/loginForm/action.ts
@@ -1,6 +1,9 @@
 "use server"
 import { Interceptor } from "@/app/_lib/interceptor";
 import { loginSchema } from "@/app/_lib/zod-schemas";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import {z} from "zod";
 
 type State = {
@@ -14,15 +17,21 @@ type State = {
 const LoginAction = async (prevState:State,formData:FormData):Promise<State>=>{
     const data = Object.fromEntries(formData)
     const validate = loginSchema.safeParse(data)
-    
+    const cookieStore = await cookies()
 
     if (!validate.success) {
         const flatten=z.flattenError(validate.error)
-        console.log(flatten,validate.error,data)
         return {errors:flatten.fieldErrors}
     }
-    const res = await Interceptor("/auth/signIn",{method:"POST",body:JSON.stringify(data)})
-    console.log(res)
-    return {}
+    const {email,password}=data
+    const res = await Interceptor<{token:string}>("/auth/signIn",{method:"POST",body:JSON.stringify({email,password})})
+    if (res.status==201) {
+        cookieStore.set("access_token",res.data?.data?.token||"")
+        redirect("/dashboard")
+    }
+    return {message:JSON.stringify(res.error)}
+
+    
+    
 }
 export default LoginAction
