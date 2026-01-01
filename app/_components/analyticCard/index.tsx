1cf680595d8420fed360813eb19171f052bf519b
TAHA RAHIMY <rahimytaha9@gmail.com>
2026-01-01 19:43:47 +0330

    /

--- app/_components/analyticCard/index.tsx
+++ app/_components/analyticCard/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { BanknoteArrowUp } from "lucide-react";
 
-type Props = {}
+type Props = {};
 
 const AnalyticCard = (props: Props) => {
   return (
-    <div>AnalyticsCard</div>
-  )
-}
+    <div className="border-border rounded-xl justify-between items-center   bg-sidebar shadow p-4  flex       border w-64  ">
+      <div>
+        <h3>$30000</h3>
+        <h4>Income Money</h4>
+      </div>
+      <BanknoteArrowUp
+        className="bg-accent/80    p-2   rounded-full  "
+        size={48}
+      />
+    </div>
+  );
+};
 
-export default AnalyticCard
+export default AnalyticCard;
